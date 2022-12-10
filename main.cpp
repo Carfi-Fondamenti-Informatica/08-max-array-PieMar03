8b1b52de9ec89c61209aadfd68269b72cefdaa34
PieMar03 <115477227+PieMar03@users.noreply.github.com>
2022-12-10 15:12:22 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+    int d=0;
+    cin>>d;
+    float a[d];
+    for(int i=0; i<d; i++){
+        cin>>a[i];
+    }
+    cout<<array(a, d)<<endl;
+    return 0;
 }
